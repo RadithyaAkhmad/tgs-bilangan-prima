718b09b500a971b8ac63ec1cdc699e44f9e4dbb4
RadithyaAkhmad <rangga.ee30@gmail.com>
2024-08-16 17:09:46 +0700

    Add files via upload

---
+++ radit/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AplikasiListBilanganPrima
+{
+    public class Program // Ubah akses kelas menjadi publik
+    {
+        public static void Main(string[] args) // Ubah akses metode Main menjadi publik
+        {
+            Console.WriteLine("Aplikasi List Bilangan Prima");
+            Console.WriteLine("========================================================================================================================");
+            Console.WriteLine("Nama : Radithya Akhmad Prianarengga");
+            Console.WriteLine("Kelas : X PPLG");
+            Console.WriteLine("Absen : 27");
+            Console.WriteLine("========================================================================================================================");
+
+            for (int num = 2; num <= 1000; num++) // Melakukan looping dari bilangan 2 hingga 1000
+            {
+                bool isPrima = true; // Menetapkan bilangan prima menjadi isPrima dan membuat isPrima menjadi true
+
+                // Melakukan loop dari 2 hingga ke akar kuadrat dari num
+                for (int pembagi = 2; pembagi <= Math.Sqrt(num); pembagi++)
+                {
+                    if (num % pembagi == 0) // Jika num habis dibagi oleh angka di dalam loop 
+                    {
+                        isPrima = false; // Maka isPrima diatur menjadi false
+                        break; // Loop dihentikan
+                    }
+                }
+
+                if (isPrima) // Jika setelah melakukan loop isPrima masih true
+                {
+                    Console.WriteLine(num + " "); // Maka num yang telah difilter akan ditampilkan ke dalam list bilangan prima pada console
+                }
+            }
+        }
+    }
+}
